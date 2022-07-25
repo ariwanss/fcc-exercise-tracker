abb5232d5f3d777a029603870e2fdc4d9f31f650
Glitch (verdant-gigantic-deer) <none>
2022-07-25 08:15:45 +0000

    branch to run on glitch

--- backend/controllers/exerciseController.js
+++ backend/controllers/exerciseController.js
@@ -24,13 +24,17 @@ const createExercise = asyncHandler(async (req, res) => {
 });
 
 const getExercises = asyncHandler(async (req, res) => {
-//   console.log(`Fetching exercise log for user: ${req.params._id}
+//  console.log(`Fetching exercise log for user: ${req.params._id}
 // ${req.query.from ? `\nfrom: ${req.query.from}` : ''}
 // ${req.query.to ? `\nto: ${req.query.to}` : ''}
 // ${req.query.limit ? `\nlimit: ${req.query.limit}` : ''}`);
 
   let { _id, username } = await getUser(req.params._id);
-  let exercises = await Exercise.find({ userId: req.params._id });
+  let exercises = await Exercise
+    .find({ userId: req.params._id, })
+    .where('date').gte(req.query.from || 0).lte(req.query.to || new Date())
+    .limit(req.query.limit || Infinity)
+    .select({ _id: 0, description: 1, duration: 1, date: 1 });
   let userLog = {
     _id,
     username,
